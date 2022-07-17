05ddaf5ab87e7bd25a3c89739e31f653acb675c1
brotherjon95 <johnsnoww1389@gmail.com>
2022-07-17 20:02:58 +0100

    Some parts from chapter 2 added.

---
+++ 02/arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] names; // can reference any array of strings
+// allocatin memory for four strings in array
+names = new string[4];
+// storing items at index positions
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebeca";
+names[3] = "Tom";
+// looping through the names
+for (int i = 0; i < names.Length; i++) {
+    // output the item at index position i
+    Console.WriteLine(names[i]);
+}
---
+++ 02/nullHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+using System;
+
+// To add OmniSharp, Ctrl + Shift + P, and select OmniSharp and Project
+
+namespace nullHandling
+{
+
+    class Address {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            // thisCannotBeNull = null; // compile error
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = null;
+            address.Region = null;
+            Console.WriteLine(address);
+        }
+    }
+}
